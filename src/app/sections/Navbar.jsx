3029d585c0899212ff70533584b81899faf16cfe
ubaid ghori <ubaid.razaghori@gmail.com>
2024-08-03 12:37:18 +0500

    UPDATED

--- src/app/sections/Navbar.jsx
+++ src/app/sections/Navbar.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
+      <div>
+        {/* LOGO */}
+      </div>
+
+
+      <div>
+        {/* NAVIEMS */}
+      </div>
       
+      <div>
+        {/* SEARCH */}
+        {/* USER */}
+        {/* CART */}
+        {/* WISHLISTS */}
+      </div>
+
+    {/* RESPONSIVE NAVBAR */}
+
     </div>
   )
 }
