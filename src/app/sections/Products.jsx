82815c0bab764517f136710daff9f1123af5ebdb
ubaid ghori <ubaid.razaghori@gmail.com>
2024-08-05 12:20:02 +0500

    updated

--- src/app/sections/Products.jsx
+++ src/app/sections/Products.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  function fetchProducts() {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then(data=>data.products)
+      setProducts(products)
+      console.log(products)
+  }
+
   return (
     <div>
-      
+      <h1>Products</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
