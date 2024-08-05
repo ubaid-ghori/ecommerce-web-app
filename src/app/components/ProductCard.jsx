493460d5afd997537368ee72355591b54d2013d4
ubaid ghori <ubaid.razaghori@gmail.com>
2024-08-05 12:02:35 +0500

    create products

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const ProductCard = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const page = () => {
   )
 }
 
-export default page
+export default ProductCard
