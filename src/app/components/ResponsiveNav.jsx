8aecf8d0a5030f6315bce2b4a60e2af94f79d891
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-07 00:15:51 -0700

    navbar completed

--- src/app/components/ResponsiveNav.jsx
+++ src/app/components/ResponsiveNav.jsx
@@ -1,32 +1,24 @@
 "use client"
 import React, { useState } from 'react';
 import { CiMenuFries } from "react-icons/ci"; 
-import { IoArrowBackOutline } from "react-icons/io"; 
-import { FaRegUser, FaCartArrowDown, FaSearch } from "react-icons/fa"; 
-import Link from 'next/link';
 import { IoClose } from "react-icons/io5";
+import Link from 'next/link';
+
 const ResponsiveNav = ({ navItems }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
-      <button onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <IoClose  className="text-2xl" /> : <CiMenuFries /> }
-      </button>
-      {isOpen && (
-        <div className="absolute top-16 left-0 w-full bg-white shadow-lg z-10">
-          {navItems.map((item, index) => (
-            <Link href={item.link} key={index}>
-              <h2 className="block px-4 py-2 text-lg font-semibold">{item.name}</h2>
-            </Link>
-          ))}
-          <div className="flex justify-around py-4">
-            <FaRegUser />
-            <FaSearch />
-            <FaCartArrowDown />
-          </div>
-        </div>
-      )}
+      <div onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <IoClose className="text-2xl" /> : <CiMenuFries className='text-2xl ' />}
+      </div>
+      <div className={`fixed h-full top-20 left-0 w-full  bg-slate-50 shadow-sm flex flex-col pt-4 gap-6  z-10 transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        {navItems.map((item, index) => (
+          <Link href={item.link} key={index}>
+            <h2 className="block px-4 py-2 text-lg font-semibold">{item.name}</h2>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
