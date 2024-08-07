700483ca17bef5a6e0d5fb655f05bae0a3694a4a
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-07 00:41:09 -0700

    updated

---
+++ src/app/Sections/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='px-20 pt-10 '>
+
+{/* 
+<div className='relative pt-24'>
+    <h1 className='text-8xl text-center opacity-75'>Find Your Perfect Shoes</h1>
+    <div className='absolute top-28 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center'>
+        <img src="https://png.pngtree.com/png-vector/20231230/ourmid/pngtree-dropshipping-men-hole-sole-jogging-shoes-png-image_11389148.png" alt="Shoes" className='w-full  h-auto max-w-lg'/>
+    </div>
+</div> */}
+
+<div>
+<div>
+    <h1 className='text-8xl text-center opacity-75'>Find Your Perfect Shoes</h1>
+           
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vitae vero distinctio enim quae explicabo ut. Recusandae, ratione sequi obcaecat.</p>
+            <button className='bg-purple-400 p-2 text-md'>Shop Now</button>
+        </div>
+        <div>
+            <img src="https://png.pngtree.com/png-vector/20231230/ourmid/pngtree-dropshipping-men-hole-sole-jogging-shoes-png-image_11389148.png" alt="" />
+        </div>
+</div>
+
+    
+    </div>
+  )
+}
+
+export default Hero
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Products from "./Products";
-import Hero from "./Hero";
+// import Hero from "./Hero";
+import Hero from "./Sections/Hero";
 import Navbar from "./Sections/Navbar";
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Hero />
     {/* <Products />
   <Hero /> */}
     </>
