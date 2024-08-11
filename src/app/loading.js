5dfd5cf96542c2c83e728428aabf2918930b493e
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-11 06:54:17 -0700

    updated

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div>
-        <h2>Loading.....</h2>
+    <div className=' flex justify-center items-center pt-20' >
+        <div class="loader">
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+</div>
+
     </div>
   )
 }
