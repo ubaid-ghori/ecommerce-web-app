163d1510250c8cd546f4bb9a1be762120dd1782e
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-11 06:54:55 -0700

    updated

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,14 +2,8 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div className=' flex justify-center items-center pt-20' >
-        <div class="loader">
-    <div class="circle"></div>
-    <div class="circle"></div>
-    <div class="circle"></div>
-    <div class="circle"></div>
-</div>
-
+    <div>
+        <h2>Loading.....</h2>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-"use client";
-import { useEffect, useState } from "react";
+"use client"
+import { useEffect } from "react";
+import Image from "next/image";
 import AOS from 'aos';
 import Hero from "./Sections/Hero";
 import 'aos/dist/aos.css';
@@ -8,37 +9,21 @@ import Brands from "./Sections/Brands";
 import ProductCategory from "./Sections/ProductCategory";
 import Sale from "./Sections/Sale";
 import Faqs from "./Sections/Faqs";
-import Loading from './Loading'; // Make sure to import your Loading component
-
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  
   useEffect(() => {
     AOS.init({
       duration: 1600,
     });
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    // Cleanup timer
-    return () => clearTimeout(timer);
   }, []);
-  
-  if (isLoading) {
-    return <Loading />; // Render loading component while loading
-  }
-  
   return (
     <>
-      <Hero />
-      <ProductCategory />
-      <Products />
-      <Brands />
-      <Sale />
-      <Faqs />
+    <Hero />
+    <ProductCategory />
+    <Products />
+    <Brands />
+    <Sale />
+    <Faqs />
+ 
     </>
   );
 }
-
-
