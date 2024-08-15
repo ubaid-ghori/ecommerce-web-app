e81a0ce965f8c16fe5578d140e85a670136404a5
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-15 05:34:47 -0700

    updated

--- src/app/(pages)/Collections/Loafers/page.jsx
+++ src/app/(pages)/Collections/Loafers/page.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import LoafersData from '../../../data/LoafersData'
-import ProductCategoryCard from '../../../components/ProductCategoryCards'
-const Loafers = () => {
-  return (
-    <div className='px-20  pt-20 '>
-      <div className='flex flex-wrap justify-center items-start gap-10'>
-        {LoafersData?.map((product,index)=>(
-          <ProductCategoryCard product={product} img={product.img} title={product.title} prevPrice={product.prevPrice} />
-        ))}
-      </div>
-    </div>
-  )
-}
+// import React from 'react'
+// import LoafersData from '../../../data/LoafersData'
+// import ProductCategoryCard from '../../../components/ProductCategoryCards'
+// const Loafers = () => {
+//   return (
+//     <div className='px-20  pt-20 '>
+//       <div className='flex flex-wrap justify-center items-start gap-10'>
+//         {LoafersData?.map((product,index)=>(
+//           <ProductCategoryCard product={product} img={product.img} title={product.title} prevPrice={product.prevPrice} />
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Loafers
+// export default Loafers
