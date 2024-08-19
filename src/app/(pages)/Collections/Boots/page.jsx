5fa5759b71578d3020f48e6be91939004279a133
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-19 05:00:00 -0700

    Add Filterd Screens

--- src/app/(pages)/Collections/Boots/page.jsx
+++ src/app/(pages)/Collections/Boots/page.jsx
@@ -1,27 +1,56 @@
-// Boots.js
 "use client";
 import React, { useState } from 'react';
 import BootsData from '../../../data/BootsData';
 import ProductCategoryCard from '../../../components/ProductCategoryCards';
 import Pagination from '../../../components/Pagination'; // Import the Pagination component
 import Link from 'next/link';
-
+import FilterCard from '../../../components/FilterCard';
 const ITEMS_PER_PAGE = 6;
 
 const Boots = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [filters, setFilters] = useState({ price: 'All' });
+
+  const applyFilters = (products) => {
+    let filteredProducts = products;
+
+    if (filters.price !== 'All') {
+      filteredProducts = filteredProducts.filter(product => {
+        const price = parseFloat(product.newPrice.replace('$', ''));
+        switch (filters.price) {
+          case '$50 to $100':
+            return price >= 50 && price <= 100;
+          case '$150 to $200':
+            return price >= 150 && price <= 200;
+          case 'Over $200':
+            return price > 200;
+          default:
+            return true;
+        }
+      });
+    }
+
+    return filteredProducts;
+  };
+
+  const filteredProducts = applyFilters(BootsData);
   const totalPages = Math.ceil(BootsData.length / ITEMS_PER_PAGE);
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
-  const currentProducts = BootsData.slice(startIndex, endIndex);
+  const currentProducts = filteredProducts.slice(startIndex, endIndex);
+ 
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
   return (
-    <div className='px-20 pt-20'>
-      <div className='flex flex-wrap gap-10'>
+    <div className='px-10 pt-20'>
+    <div className='lg:flex block'>
+    <div className='w-[40%]'>
+          <FilterCard setFilters={setFilters} />
+        </div>
+      <div className='flex flex-wrap justify-center gap-5 pt-10'>
         {currentProducts.map((product, index) => (
          <Link href={`/Boots/${product.id}`} key={index}>
           <ProductCategoryCard 
@@ -34,6 +63,7 @@ const Boots = () => {
          </Link>
         ))}
       </div>
+    </div>
 
       <Pagination 
         currentPage={currentPage} 
--- src/app/(pages)/Collections/Loafers/page.jsx
+++ src/app/(pages)/Collections/Loafers/page.jsx
@@ -5,23 +5,54 @@ import LoafersData from '../../../data/LoafersData';
 import ProductCategoryCard from '../../../components/ProductCategoryCards';
 import Pagination from '../../../components/Pagination'; 
 import Link from 'next/link';
+import FilterCard from '../../../components/FilterCard';
 
 const ITEMS_PER_PAGE = 6;
 
 const Loafers = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [filters, setFilters] = useState({ price: 'All' });
+
+  const applyFilters = (products) => {
+    let filteredProducts = products;
+
+    if (filters.price !== 'All') {
+      filteredProducts = filteredProducts.filter(product => {
+        const price = parseFloat(product.newPrice.replace('$', ''));
+        switch (filters.price) {
+          case '$50 to $100':
+            return price >= 50 && price <= 100;
+          case '$150 to $200':
+            return price >= 150 && price <= 200;
+          case 'Over $200':
+            return price > 200;
+          default:
+            return true;
+        }
+      });
+    }
+
+    return filteredProducts;
+  };
+
+  const filteredProducts = applyFilters(LoafersData);
   const totalPages = Math.ceil(LoafersData.length / ITEMS_PER_PAGE);
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
-  const currentProducts = LoafersData.slice(startIndex, endIndex);
+  const currentProducts = filteredProducts.slice(startIndex, endIndex);
+
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
   return (
-    <div className='px-20 pt-20'>
-      <div className='flex flex-wrap justify-center items-start gap-10'>
+    <div className='lg:px-10 px-4 pt-20'>
+        <div className='lg:flex block'>
+        <div className='w-[40%]'>
+          <FilterCard setFilters={setFilters} />
+        </div>
+      <div className='flex flex-wrap justify-center gap-5 pt-10'>
         {currentProducts.map((product, index) => (
           <Link key={index} href={`/Loafers/${product?.id}`}>
           <ProductCategoryCard 
@@ -33,6 +64,7 @@ const Loafers = () => {
           />
          </Link>
         ))}
+      </div>
       </div>
 
       <Pagination 
