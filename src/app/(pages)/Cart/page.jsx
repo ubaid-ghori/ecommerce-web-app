e15bdd88ad218be4d7a15545093eb114ae91acd3
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-30 03:52:22 -0700

    Create and Style Cart Page

--- src/app/(pages)/Cart/page.jsx
+++ src/app/(pages)/Cart/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Button from '../../components/Button'
+import Link from 'next/link'
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="pt-20 flex flex-col  justify-center">
+      <h2 className=" text-Text text-4xl font-bold font-body text-center ">
+      Your cart is empty
+      </h2>
+      <div className="flex justify-center items-center mt-10">
+        <Button>
+          <Link href="/">Continue Shopping</Link>
+        </Button>
+      </div>
+      <div>
+        <h2 className=" text-Text text-xl font-bold font-heading mt-10 text-center">Have an account?</h2>
+        <p className="text-Text font-medium font-body text-md mt-2 text-center"> <a className=" underline" href="/">Log in</a> to check out faster.</p>
+      </div>
+    </div>
+    
+  );
+};
 
-export default Cart
+export default Cart;
