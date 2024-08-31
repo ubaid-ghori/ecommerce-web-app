a39350d65266bbb9e3ae3bec08051ac81814d704
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-31 05:25:07 -0700

    updated

--- src/app/components/CartItem.jsx
+++ src/app/components/CartItem.jsx
@@ -1,16 +1,18 @@
-// "use clinet"
-// import React,{useState,useEffect} from 'react'
-// import {products} 
-// const CartItem = ({id,image,price}) => {
-//     const {id,image,price} = props
-//     const [productId,quantity]=props.data
-//     const [detail,setdetails]=useState([])
-//     useEffect(()=>{
-//     const findDetails=products.fil
-//     },[productId])
-//   return (
-//     <div>CartItem</div>
-//   )
-// }
+"use clinet"
+import React,{useState,useEffect} from 'react'
+import { ProductCategoryData } from '../data/ProductCategoryData' 
+const CartItem = ({id,image,price}) => {
+    const {id,image,price} = props
+    const [productId,quantity]=props.data
+    const [detail,setdetails]=useState([])
+    useEffect(()=>{
+    const findDetails= ProductCategoryData.find((item)=>item.id===productId)[0]
+    setdetails(findDetails)
+    },[productId])
+    console.log(detail)
+  return (
+    <div>CartItem</div>
+  )
+}
 
-// export default CartItem
+export default CartItem
