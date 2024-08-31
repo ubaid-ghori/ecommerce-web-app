7e5b520daae1a36fc1cdb5ef4243c6ac0bea5c86
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-08-31 03:28:15 -0700

    updated

--- src/app/Stores/cart.js
+++ src/app/Stores/cart.js
@@ -2,24 +2,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  items: []
+    items: []
 };
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-    addToCart: (state, action) => {
-      const { productId, quantity } = action.payload;
-      const existingProduct = state.items.find(item => item.productId === productId);
-      if (existingProduct) {
-        existingProduct.quantity += quantity;
-      } else {
-        state.items.push({ productId, quantity });
-      }
+    name: 'cart',
+    initialState,
+    reducers: {
+       addToCart:(state,action)=>{
+        const {productId,quantity} = action.payload;
+        const indexProductId=(state.items.findIndex(item=>item.productId===productId));
+        if(indexProductId>=0){
+            state.items[indexProductId].quantity+=quantity;
+        }
+        else{
+        state.items.push({productId,quantity})
+        }
+       }
     }
-  }
 });
-
-export const { addToCart } = cartSlice.actions;
+export const {addToCart} = cartSlice.actions;
 export default cartSlice.reducer;
--- src/app/components/DetailCard.jsx
+++ src/app/components/DetailCard.jsx
@@ -1,12 +1,11 @@
 "use client"
 import React, { useState, useRef } from 'react';
 import Image from 'next/image';
-import { CircleCheckBig, Heart, Share2, Plus, Minus } from 'lucide-react';
+import { CircleCheckBig, Heart, Share, Share2,Plus,Minus } from 'lucide-react';
 import Button from './Button';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../Stores/cart';
-
-const DetailCard = ({ title, image, desc, price, btnText, id }) => {
+const DetailCard = ({ title, image, desc, price, btnText,id }) => {
   const [zoom, setZoom] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const imageRef = useRef(null);
@@ -23,72 +22,72 @@ const DetailCard = ({ title, image, desc, price, btnText, id }) => {
   const handleMouseEnter = () => setZoom(true);
   const handleMouseLeave = () => setZoom(false);
 
-  const handleMinus = () => setQuantity((prev) => (prev > 1 ? prev - 1 : 1));
-  const handlePlus = () => setQuantity((prev) => prev + 1);
+  const hanldeMinus=()=>{
+    setQuantity(quantity-1 < 1 ? 1 : quantity - 1 )
+  }
+
+  const hanldePlus=()=>{
+    setQuantity(quantity + 1)
+  }
 
   const handleAddToCart = () => {
-    dispatch(addToCart({ productId: id, quantity }));
-  };
-
+    dispatch(addToCart({
+      productId:id,
+      quantity:quantity
+    }))
+  }
   return (
     <div className="md:px-20 px-4 pt-20">
-      <div className="lg:flex block gap-20 items-center">
-        <div
-          className="relative w-full lg:w-2/4"
-          onMouseMove={handleMouseMove}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
+      <div className="lg:flex block gap-20 items-center ">
+        <div className="relative w-full lg:w-2/4" onMouseMove={handleMouseMove} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <div className={`overflow-hidden rounded-lg ${zoom ? 'cursor-zoom-in' : ''}`}>
             <Image
               src={image}
               width={800}
               height={500}
-              className={`rounded-lg transition-transform duration-300 ease-in-out ${zoom ? 'scale-150' : 'scale-100'}`}
+              className={`rounded-lg   transition-transform duration-300 ease-in-out ${zoom ? 'scale-150' : 'scale-100'}`}
               ref={imageRef}
               alt={title}
-              style={{ transformOrigin: `${position.x}% ${position.y}%` }}
+              style={{
+                transformOrigin: `${position.x}% ${position.y}%`,
+              }}
             />
           </div>
+        
         </div>
-        <div className="flex flex-col gap-7 lg:gap-12 mt-10 lg:mt-0">
-          <h3 className="lg:text-5xl text-3xl font-heading font-medium text-Text">{title}</h3>
-          <p className="font-semibold text-Text text-md lg:max-w-md">{desc}</p>
-          <h3 className="text-xl text-bgColor font-semibold">
-            <span className="text-Text">Rs:</span> {price}
-          </h3>
-          <div className="flex flex-wrap gap-5 text-Text font-semibold items-center cursor-pointer">
-            <div className="flex gap-2 hover:text-bgColor">
-              <Heart />
-              <h2>Add To Wishlist</h2>
-            </div>
-            <div className="flex gap-2 hover:text-bgColor">
-              <CircleCheckBig />
-              <h2>Add To Compare</h2>
-            </div>
-            <div className="flex gap-2 hover:text-bgColor">
-              <Share2 />
-              <h2>Share</h2>
-            </div>
+        <div className=' flex flex-col gap-7 lg:gap-12 mt-10 lg:mt-0'>
+          <h3 className="lg:text-5xl text-3xl font-heading font-medium text-Text ">{title}</h3>
+          <p className=" font-semibold text-Text text-md   lg:max-w-md">Nunc vehicula quam semper odio varius tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posue.</p>
+          <h3 className=" text-xl text-bgColor font-semibold"> <span className=' text-Text'>Rs:</span> {price}</h3>
+         <div className='flex flex-wrap gap-5 text-Text font-semibold  items-center cursor-pointer '>
+         <div className='flex gap-2 hover:text-bgColor  '>
+            <Heart />
+            <h2>Add To Wishlists</h2>
           </div>
-          <div className="lg:flex block gap-4">
-            <div className="flex cursor-pointer w-full lg:w-36 justify-center items-center gap-4 border rounded-full p-2 border-Text lg:mb-0 mb-5">
-              <Plus onClick={handlePlus} />
-              <h2>{quantity}</h2>
-              <Minus onClick={handleMinus} />
-            </div>
-            <div>
-              <Button
-                className={`!bg-white hover:bg-bgColor !text-Text border-2 border-Text`}
-                onClick={handleAddToCart}
-              >
-                Add To Cart
-              </Button>
-            </div>
+          <div className='flex gap-2 hover:text-bgColor '>
+            <CircleCheckBig />
+            <h2>Add To Compare</h2>
           </div>
-          <div>
-            <Button>Buy it Now</Button>
+          <div className='flex gap-2 hover:text-bgColor '>
+            <Share2 />
+            <h2>Share</h2>
           </div>
+         </div>
+         <div className='lg:flex block gap-4 '>
+         <div className='flex cursor-pointer w-full lg:w-36 justify-center items-center gap-4 border rounded-full p-2 border-Text lg:mb-0 mb-5'> 
+            <Plus onClick={hanldePlus} />
+            <h2>{quantity}</h2>
+            <Minus onClick={hanldeMinus} />
+         </div>
+         <div>
+            <Button className={`!bg-white hover:bg-bgColor !text-Text border-2 border-Text`}
+            onClick={handleAddToCart}
+            >Add To Cart</Button>
+         </div>
+         </div>
+         <div className=''>
+            <Button>But it Now</Button>
+         </div>
         </div>
       </div>
     </div>
