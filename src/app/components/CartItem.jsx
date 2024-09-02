211c2f38e3fb33cdae2ebd74e1dd70bb8c2c85e2
ubaid-ghori <ubaid.razaghori@gmail.com>
2024-09-02 00:54:58 -0700

    updated CartItems

--- src/app/components/CartItem.jsx
+++ src/app/components/CartItem.jsx
@@ -1,13 +1,13 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import { ProductCategoryData } from '../data/ProductCategoryData';
+import SportsData from '../data/SportsData'
 
 const CartItem = ({ data }) => {
   const { productId, quantity } = data;
-  const [detail, setDetail] = useState({});
+  const [detail, setDetail] = useState([]);
 
   useEffect(() => {
-    const findDetails = ProductCategoryData.find((item) => item.id === productId);
+    const findDetails = SportsData.find((item) => item.id === productId);
     setDetail(findDetails);
   }, [productId]);
   console.log(detail);
