9d9fe534ff2b5741d75ebf0b708c71757deb497a
ubaid ghori <ubaid.razaghori@gmail.com>
2024-09-05 11:14:38 -0700

    updated

--- src/app/(pages)/Cart/page.jsx
+++ src/app/(pages)/Cart/page.jsx
@@ -6,29 +6,36 @@ import { useSelector } from "react-redux";
 import CartItem from "../../components/CartItem";
 
 const Cart = () => {
-  const carts = useSelector(store => store.cart.items);
+  const carts = useSelector((store) => store.cart.items);
 
   return (
-    <div className="pt-20 flex flex-col justify-center">
+    <div className="pt-20 flex flex-col justify-center items-center">
       <div>
         {carts.length ? (
           carts.map((item, index) => <CartItem key={index} data={item} />)
         ) : (
-          <h2 className="text-Text text-4xl font-bold font-body text-center">
-            Your cart is empty
-          </h2>
+          <div className="text-center">
+            <h2 className="text-Text text-4xl font-bold font-body">
+              Your cart is empty
+            </h2>
+            <div className="flex justify-center items-center mt-10">
+              <Link href="/" passHref>
+                <Button>Continue Shopping</Button>
+              </Link>
+            </div>
+            <div>
+              <h2 className="text-Text text-xl font-bold font-heading mt-10">
+                Have an account?
+              </h2>
+              <p className="text-Text font-medium font-body text-md mt-2">
+                <Link href="/" className="underline">
+                  Log in
+                </Link>{" "}
+                to check out faster.
+              </p>
+            </div>
+          </div>
         )}
-      </div>
-      <div className="flex justify-center items-center mt-10">
-        <Button>
-          <Link href="/">Continue Shopping</Link>
-        </Button>
-      </div>
-      <div>
-        <h2 className="text-Text text-xl font-bold font-heading mt-10 text-center">Have an account?</h2>
-        <p className="text-Text font-medium font-body text-md mt-2 text-center">
-          <a className="underline" href="/">Log in</a> to check out faster.
-        </p>
       </div>
     </div>
   );
