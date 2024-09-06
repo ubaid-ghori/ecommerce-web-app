2fee7cc2a523db90b95455362a1bc1d461b5478b
= <=>
2024-09-06 03:29:17 -0700

    updated

--- src/app/components/SkeletonCard.jsx
+++ src/app/components/SkeletonCard.jsx
@@ -3,7 +3,7 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 const SkeletonCard = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full lg:px-20 p-4 mt-20">
        <Skeleton height={40} width={`60%`} className="mb-4" />
        <Skeleton count={5} height={25} className="mb-2" />
     </div>
