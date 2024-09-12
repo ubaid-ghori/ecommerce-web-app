a8d3e17e08d878d56b513b73fe4a074b4f0d7501
ubaid ghori <ubaid.razaghori@gmail.com>
2024-09-12 09:40:36 -0700

    updated

--- src/app/components/userInfo.jsx
+++ src/app/components/userInfo.jsx
@@ -2,9 +2,16 @@
 
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
-
+import { useRouter } from "next/navigation";
 export default function UserInfo() {
   const { data: session } = useSession();
+
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push("/");
+  };
 
   return (
     <div className="grid place-items-center h-screen">
@@ -16,7 +23,7 @@ export default function UserInfo() {
           Email: <span className="font-bold">{session?.user?.email}</span>
         </div>
         <button
-          onClick={() => signOut()}
+          onClick={handleSignOut}
           className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
         >
           Log Out
