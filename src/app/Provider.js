ffd468a1c84c1961b3be768ffb410c4d8f83290e
ubaid ghori <ubaid.razaghori@gmail.com>
2024-09-12 04:15:09 -0700

    updated

---
+++ src/app/Provider.js
@@ -0,0 +1,10 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+
+export const AuthProvider = ({ children }) => {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
